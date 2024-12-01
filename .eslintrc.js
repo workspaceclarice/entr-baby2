59aeb929f403988742f4e77fa1ec8fef661eca02
workspaceclarice <125782127+workspaceclarice@users.noreply.github.com>
2024-11-30 16:58:12 -0800

    v8 w vercel deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
   extends: ['react-app'],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
     'react/react-in-jsx-scope': 'off'
   }
